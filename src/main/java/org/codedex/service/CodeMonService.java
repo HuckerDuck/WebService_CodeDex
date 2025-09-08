d9af18103fa95e7f9cf62abd65d99fb3e7faa065
EricBO2 <eric.carlsson@stud.sti.se>
2025-09-08 14:59:44 +0200

    Add new data classes
    Add CodeMonDTO class, codeMonTDOToCodeMon function and moved validaton from CodeMon to CodeMonDTO

--- src/main/java/org/codedex/service/CodeMonService.java
+++ src/main/java/org/codedex/service/CodeMonService.java
@@ -1,5 +1,22 @@
 package org.codedex.service;
 
+import org.codedex.Model.CodeMon;
+import org.codedex.Model.CodeMonDTO;
+
 //? När projektet blir större kan vi eventuellt lägga all logik här istället
 public class CodeMonService {
+
+
+
+
+
+
+    private CodeMon codeMonDTOToCodeMon(CodeMonDTO codeMonDTO) {
+        CodeMon codeMon = new CodeMon();
+        codeMon.setName(codeMonDTO.name());
+        codeMon.setCodeMonGeneration(codeMonDTO.codeMonGeneration());
+        codeMon.setHp(codeMonDTO.hp());
+        codeMon.setAttackdmg(codeMonDTO.attackdmg());
+        return codeMon;
+    };
 }
