20e371aecc78bfc5794ee6d4cf3d4673791f4738
HuckerDuck <frmb1@hotmail.com>
2025-09-08 11:14:18 +0200

    - Updated the controller - Added a Patchmetod - Added a delete method

--- src/main/java/org/codedex/controller/JavaMonController.java
+++ src/main/java/org/codedex/controller/JavaMonController.java
@@ -2,6 +2,7 @@ package org.codedex.controller;
 
 import org.codedex.Model.JavaMon;
 import org.codedex.Repository.JavaMonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,13 +11,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/javamons")
 public class JavaMonController {
-    //? Injisering av repo
-    private final JavaMonRepository javaMonRepository;
+    @Autowired
+    JavaMonRepository javaMonRepository;
 
-    //? Konstruktor
-    public JavaMonController(JavaMonRepository javaMonRepository) {
-        this.javaMonRepository = javaMonRepository;
-    }
 
     //? Metod för att hämta alla
     @GetMapping
@@ -26,9 +23,10 @@ public class JavaMonController {
 
     //? Metod för att hämta en specifik
     @GetMapping("/{id}")
-    public JavaMon getASpecfic(@PathVariable String id ){
-        return javaMonRepository.findById(id).orElseThrow
-                (()-> new IllegalArgumentException("No JavaMon found with id " + id));
+    public ResponseEntity <JavaMon> findJavaMonbyID(@PathVariable String id){
+        return javaMonRepository.findById(id)
+                .map(JavaMon -> ResponseEntity.ok(JavaMon))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     //? Metod för att lägga till en Javamon
@@ -36,7 +34,53 @@ public class JavaMonController {
     public ResponseEntity <JavaMon> save(@RequestBody JavaMon javaMon){
            JavaMon savedJavaMon = javaMonRepository.save(javaMon);
            return ResponseEntity.status(201).body(savedJavaMon);
+
+           //
     }
 
+    //? Uppdatera delar av en Javamon
+    @PatchMapping("/{id}")
+    public ResponseEntity <JavaMon> updateAStudent(@PathVariable String id, @RequestBody
+    JavaMon JavaMonInformation){
+        return javaMonRepository.findById(id)
+                .map (JavaMon -> {
+
+
+                    //! Kolla att namnet inte är tomt
+                    if (JavaMonInformation.getName() != null){
+                        JavaMon.setName(JavaMonInformation.getName());
+                    }
+                    //! Kolla att typen av Javamon inte är tomt
+                    if (JavaMonInformation.getType() != null){
+                        JavaMon.setType(JavaMonInformation.getType());
+                    }
+                    //! Kolla att attack skadan av Javamon inte är tomt
+                    if (JavaMonInformation.getAttackdmg() != null){
+                        JavaMon.setAttackdmg(JavaMon.getAttackdmg());
+                    }
+                    //! Kolla att hp:t av Javamon inte är tomt
+                    if (JavaMonInformation.getHp() != null){
+                        JavaMon.setHp(JavaMon.getHp());
+                    }
+
+                    //! Om allt detta är okej så ska den då spara en student
+                    JavaMon updatedJavaMon = javaMonRepository.save(JavaMon);
+                    return ResponseEntity.ok(updatedJavaMon);
+                })
+                //? Om studenten inte finns, returnera ett 404 Not Found svar
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //? Ta bort en JavaMon med id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAStudent(@PathVariable String id){
+        return javaMonRepository.findById(id)
+                .map (JavaMon -> {
+                    javaMonRepository.delete(JavaMon);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                //? Om Javamon inte finns, returnera ett 404 Not Found svar
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
