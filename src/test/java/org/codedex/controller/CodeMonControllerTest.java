9869f6019635a21f9930135c3957a0cf0a75d660
HuckerDuck aka Fredrik <fredrikmenotbrauer@gmail.com>
2025-09-16 15:37:11 +0200

    - Added a test to try the method that I added. - Using mock to mock the controller and added 3 codemons to a Page List - I expected the response to be OK, and to have the codemons in the right order. - Added a @TestConfiguration to make a Service so I don't have to use the real one. - That service would contact the database and that wouldn't be good.
    Might add more more test later

--- src/test/java/org/codedex/controller/CodeMonControllerTest.java
+++ src/test/java/org/codedex/controller/CodeMonControllerTest.java
@@ -1,57 +1,124 @@
 package org.codedex.controller;
 
 import org.codedex.Model.CodeMon;
-import org.codedex.Repository.CodeMonRepository;
+
+import org.codedex.service.CodeMonService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import java.util.List;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(CodeMonController.class)
+@Import(CodeMonControllerTest.TestConfig.class)
 public class CodeMonControllerTest {
-    //? Mock av vårat CodeMonRepository
-    //? Blir som ett låtsasrepo
-    @Mock
-    private CodeMonRepository codeMonRepository;
 
-    //? Används så att Mockito automatiskt stoppar in vårt
-    //? "låtsas-repo" i våran service-klass
-    @InjectMocks
-    private CodeMonController codeMonController;
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        CodeMonService codeMonService() {
+            return Mockito.mock(CodeMonService.class);
+        }
+    }
 
-    private CodeMon testCodeMon;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CodeMonService codeMonService;
+
+    //? Vi behöver 3 codemons som vi testar mot
+    private CodeMon testCodeMonA;
+    private CodeMon testCodeMonB;
+    private CodeMon testCodeMonC;
+
 
     @BeforeEach
-    //? BeforeEach = Före varje test så körs detta
-    void setUp(){
-        //? Skapar 3 codemon vi kommer att kunna testa mot
-        testCodeMon = new CodeMon();
-        testCodeMon.setName("TestCodeMon");
-        testCodeMon.setType("TestingType");
-        testCodeMon.setCodeMonGeneration(1);
-        testCodeMon.setHp(100);
-        testCodeMon.setAttackdmg(20);
+    //? Vi skapar de tre codemons som vi testar mot
+    //? Dessa kommer vi att försöka sortera med
+    void setUp() {
+        testCodeMonA = new CodeMon();
+        testCodeMonA.setName("TestCodeMonA");
+        testCodeMonA.setType("TestTypeA");
+        testCodeMonA.setCodeMonGeneration(1);
+        testCodeMonA.setAttackdmg(10);
+        testCodeMonA.setHp(100);
 
-        testCodeMon = new CodeMon();
-        testCodeMon.setName("TestCodeMon2");
-        testCodeMon.setType("TestingType2");
-        testCodeMon.setCodeMonGeneration(1);
-        testCodeMon.setHp(110);
-        testCodeMon.setAttackdmg(30);
+        //? Denna har mest HP
+        testCodeMonB = new CodeMon();
+        testCodeMonB.setName("TestCodeMonB");
+        testCodeMonB.setType("TestTypeB");
+        testCodeMonB.setCodeMonGeneration(1);
+        testCodeMonB.setAttackdmg(50);
+        testCodeMonB.setHp(120);
 
-        testCodeMon = new CodeMon();
-        testCodeMon.setName("TestCodeMon3");
-        testCodeMon.setType("TestingType3");
-        testCodeMon.setCodeMonGeneration(1);
-        testCodeMon.setHp(120);
-        testCodeMon.setAttackdmg(40);
+        //? Denna har mest attackdmg
+        testCodeMonC = new CodeMon();
+        testCodeMonC.setName("TestCodeMonC");
+        testCodeMonC.setType("TestTypeC");
+        testCodeMonC.setCodeMonGeneration(1);
+        testCodeMonC.setAttackdmg(30);
+        testCodeMonC.setHp(200);
+    }
+
+    @Test
+    void testGetCodeMonsByGenerationAndSorting() throws Exception {
+        // Skapande av page och listan
+        Page<CodeMon> page = new PageImpl<>(
+                //? Vi skapar en lista med de tre codemons vi skapade ovan
+                //? Vi kommer att använda detta i testet nedan sedan
+                List.of(testCodeMonB, testCodeMonC, testCodeMonA),
+                PageRequest.of(0,3 ),
+                3
+        );
+
+        when(codeMonService.getCodeMonsByGenerationAndSorting(
+                //? Detta betyder att vi testar mot generation 1
+                eq(1),
+
+                //? Vi använder isNull för att vi vill testa de fall
+                //? Där användaren inte väljer att sortera på något exakt
+                //? Då är det null som skickas vilket vi har godkänt från controllern
+                isNull(),
+
+                //?
+                any(Pageable.class)
+        )).thenReturn(page);
+
+        //? Vi testar att mocka mot en server med URL:en nedan
+        //? Vi låtsas ha controllern igång
+        mockMvc.perform(get("/api/javamons/generation/{gen}/top", 1)
+        )
+                // Vi föräntar oss att
+                .andExpect(status().isOk())
+                // Vi förväntar oss att det finns 3 objekt i
+                //? Vi förväntar oss sedan att första attack numret är 50
+                .andExpect(jsonPath("$.content[0].attackdmg").value(50))
+                .andExpect(jsonPath("$.content[1].attackdmg").value(30))
+                .andExpect(jsonPath("$.content[2].attackdmg").value(10));
+
+
     }
 
 
 
 
-
-
-
 }
+
+
